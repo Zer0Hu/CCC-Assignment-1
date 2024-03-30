b31a93d416f880ed7237b92a04cdeb8432e6878e
Zer0Hu <140781099+Zer0Hu@users.noreply.github.com>
2024-03-30 11:41:46 +1100

    Add files via upload

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import time
 import numpy as np
 import re
 from Utils import read_file, extract_sentiment, extract_created_at
+from Utils import calculate_q1,calculate_q2
 
 # Read files
 file_1 = 'twitter-1mb.json'
@@ -35,6 +36,18 @@ print("run time:", time.time() - t0) # Using the time when finish reading
 # minus initial time to record the time needed to read the file
 
 
-# 
+
+# Question 1
+max_sentiment, happiest_hour_index = calculate_q1(hour_sentiment)
+print("The happiest hour ever is at {}-{} {}:00, with a sentiment score of {:.2f}".format(
+    happiest_hour_index[0], happiest_hour_index[1], happiest_hour_index[2], max_sentiment))
+
+
+# Question 2
+happiest_day, happiest_day_index = calculate_q2(hour_sentiment)
+print("The happiest day ever is at {}-{}, with a total sentiment score of {:.2f}".format(
+    happiest_day_index[0]+1, happiest_day_index[1]+1, happiest_day))
+
+
 
 
--- Utils.py
+++ Utils.py
@@ -1,5 +1,5 @@
 import time
-import numpy
+import numpy as np
 import re
 from datetime import datetime
 
@@ -49,6 +49,32 @@ def extract_created_at(line):
     else:
         return None
     
+def calculate_q1(hour_sentiment):
+    max_sentiment = 0  # 初始设为0
+    happiest_hour_index = None  # 最快乐的小时的index
+
+    
+    for month in range(hour_sentiment.shape[0]):
+        for day in range(hour_sentiment.shape[1]):
+            for hour in range(hour_sentiment.shape[2]):
+                sentiment = hour_sentiment[month, day, hour]
+                # 如果当前小时的sentiment大于max_sentiment，则更新max_sentiment和index
+                if sentiment > max_sentiment:
+                    max_sentiment = sentiment
+                    happiest_hour_index = (month + 1, day + 1, hour)
+
+    return max_sentiment, happiest_hour_index
+
+
+def calculate_q2(hour_sentiment):
+    # 将第三维度（小时）的值加起来，得到每天的total sentiment
+    day_sentiment_total = np.sum(hour_sentiment, axis=2)
+    
+    # 找到sentiment最大的一天和对应的index
+    happiest_day = np.max(day_sentiment_total)
+    happiest_day_index = np.unravel_index(np.argmax(day_sentiment_total), day_sentiment_total.shape)
+    
+    return happiest_day, happiest_day_index
 
 
 
